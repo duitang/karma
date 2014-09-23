ccc8408dafbc802582ce491ae2dd6c4472417172
laurence <laurence@duitang.com>
2014-09-24 01:03:59 +0800

    remove useless import

--- src/main/java/com/duitang/service/base/AbstractClientFactory.java
+++ src/main/java/com/duitang/service/base/AbstractClientFactory.java
@@ -11,19 +11,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.avro.ipc.specific.SpecificRequestor;
 import org.apache.log4j.Logger;
 
-import com.codahale.metrics.Histogram;
-import com.codahale.metrics.Meter;
-
 public abstract class AbstractClientFactory<T> implements ServiceFactory<T> {
 
 	protected Logger err = Logger.getLogger("error");
 
 	protected String url;
 	protected List<URL> serviceURL;
-	protected Meter qps;
-	protected Histogram dur;
-	protected Meter qps_f;
-	protected Histogram dur_f;
 	protected AtomicInteger hashid = new AtomicInteger(0);
 	protected int sz;
 	protected int timeout = 500;
