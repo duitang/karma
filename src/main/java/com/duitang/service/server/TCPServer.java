7197277c8609bad2ffc3bd81d8f60b6893aca5c2
laurence <laurence@duitang.com>
2014-12-27 16:13:58 +0800

    tuning: server-side pool

--- src/main/java/com/duitang/service/server/TCPServer.java
+++ src/main/java/com/duitang/service/server/TCPServer.java
@@ -3,7 +3,9 @@ package com.duitang.service.server;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.mina.core.service.SimpleIoProcessorPool;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
@@ -20,13 +22,12 @@ public class TCPServer implements RPCService {
 	final static int DEFAULT_TCP_PORT = 7778;
 	// protected Executor pool1 = new ThreadPoolExecutor(2, 16, 60,
 	// TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
-	protected Executor pool = Executors.newCachedThreadPool();
-	// protected Executor pool2 = new ThreadPoolExecutor(2, 8, 300,
-	// TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+	protected Executor pool = new ThreadPoolExecutor(5, 200, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
+	protected Executor pool2 = new ThreadPoolExecutor(2, 6, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
 	// protected Executor pool3 = new ThreadPoolExecutor(10, 150, 60,
 	// TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
-	protected SimpleIoProcessorPool proc = new SimpleIoProcessorPool(NioProcessor.class, pool);
-	protected NioSocketAcceptor acceptor = new NioSocketAcceptor(proc);
+	protected SimpleIoProcessorPool proc = new SimpleIoProcessorPool(NioProcessor.class, pool2);
+	protected NioSocketAcceptor acceptor = new NioSocketAcceptor(pool, proc);
 	protected Router router;
 
 	protected int port = DEFAULT_TCP_PORT;
@@ -44,8 +45,6 @@ public class TCPServer implements RPCService {
 		acceptor.setReuseAddress(true);
 		acceptor.getSessionConfig().setTcpNoDelay(true);
 		// acceptor.getSessionConfig().setKeepAlive(true);
-		// acceptor.getFilterChain().addLast("threadPool", new
-		// ExecutorFilter(pool));
 		acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new KarmaBinaryCodecFactory()));
 		JavaServerHandler handler = new JavaServerHandler();
 		handler.setRouter(router);
