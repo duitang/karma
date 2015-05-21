751496cad87cd78a6cf92ed7031a4eb5c68b9d1d
kevx <kevx@kevx-mac.local>
2015-05-21 18:40:11 +0800

    .

--- src/main/java/com/duitang/service/karma/client/WRRBalancer.java
+++ src/main/java/com/duitang/service/karma/client/WRRBalancer.java
@@ -134,8 +134,9 @@ public class WRRBalancer implements IOBalance {
                     if (cw <= 0) {
                         cw = Collections.max(serverWt.values());
                         if (cw == 0) {
-                            System.out.println("wrr_shit:" + serverWt.size() + serverWt.keySet().toArray(new String[0]).length);
-                            return null;
+                            String[] ss = serverWt.keySet().toArray(new String[0]);
+                            System.out.println("wrr_shit:"+ ss[0]);
+                            return ss[0];
                         }
                     }
                 } 
