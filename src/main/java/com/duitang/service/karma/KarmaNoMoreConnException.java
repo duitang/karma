017ea14359cd48d55d01dcf40090b3f15f85a8c6
kevx <kevx@kevx-mac.local>
2015-05-21 19:37:04 +0800

    .

---
+++ src/main/java/com/duitang/service/karma/KarmaNoMoreConnException.java
@@ -0,0 +1,15 @@
+package com.duitang.service.karma;
+
+/**
+ * 
+ * @author kevx
+ * @since 7:35:36 PM May 21, 2015
+ */
+public class KarmaNoMoreConnException extends KarmaOverloadException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -3338950361429293389L;
+
+}
