97880a3c78a2a0ecba4dc1f30aa7529f091fd64f
Yan Su <yan.su@duitang.com>
2015-05-22 14:04:17 +0800

    .

--- src/main/java/com/duitang/service/karma/base/MetricCenter.java
+++ src/main/java/com/duitang/service/karma/base/MetricCenter.java
@@ -10,33 +10,33 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MetricCenter {
 
-	final static Logger out = Logger.getLogger(MetricCenter.class);
+	final static Logger logger = LoggerFactory.getLogger(MetricCenter.class);
 	final public static boolean debug = false;
 
 	final static String[] NOT_IN_PACKAGE_NAME = { "com.duitang.service.karma" };// "com.duitang.webx",
 
-	static MetricReportDaemon daemon = null;
+//	static MetricReportDaemon daemon = null;
 	static protected String hostname = null;
 	public static Map<String, MetricUnit> method_dur = new HashMap<String, MetricUnit>();
 
-	final static Reporter console = new Reporter() {
-		@Override
-		public void report(Map data) {
-			System.out.println(data);
-		}
-	};
-
-	static {
-		daemon = new MetricReportDaemon();
-		Thread t = new Thread(daemon);
-		t.setDaemon(true);
-		t.start();
-	}
+//	final static Reporter console = new Reporter() {
+//		@Override
+//		public void report(Map data) {
+//			System.out.println(data);
+//		}
+//	};
+//
+//	static {
+//		daemon = new MetricReportDaemon();
+//		Thread t = new Thread(daemon);
+//		t.setDaemon(true);
+//		t.start();
+//	}
 
 	public static void methodMetric(String clientId, String name, long elapse) {
 		methodMetric(clientId, name, elapse, false);
@@ -112,96 +112,96 @@ public class MetricCenter {
 		}
 	}
 
-	static public void alterReportPeroid(long peroid) {
-		daemon.peroid = peroid;
-	}
+//	static public void alterReportPeroid(long peroid) {
+//		daemon.peroid = peroid;
+//	}
+//
+//	static public void enableConsoleReporter(boolean openit) {
+//		if (openit) {
+//			daemon.rps.add(console);
+//		} else {
+//			daemon.rps.remove(console);
+//		}
+//	}
+//
+//	static public void enableKafkaReporter(Properties config) {
+//		Reporter kafkaReporter = new KafkaJsonReporter(config);
+//		daemon.rps.add(kafkaReporter);
+//	}
+//
+//	static public void addLoggerReporter(final Logger logR) {
+//		Reporter logReport = new Reporter() {
+//			@Override
+//			public void report(Map data) {
+//				logR.info(data.toString());
+//			}
+//		};
+//		daemon.rps.add(logReport);
+//	}
 
-	static public void enableConsoleReporter(boolean openit) {
-		if (openit) {
-			daemon.rps.add(console);
-		} else {
-			daemon.rps.remove(console);
-		}
-	}
+//	static public void addLoggerReporterByName(String name) {
+//		final Logger logger = Logger.getLogger(name);
+//		logger.setLevel(Level.INFO);
+//		Reporter logReport = new Reporter() {
+//			Logger logR = logger;
+//
+//			@Override
+//			public void report(Map data) {
+//				logR.info(data.toString());
+//			}
+//		};
+//		daemon.rps.add(logReport);
+//	}
 
-	static public void enableKafkaReporter(Properties config) {
-		Reporter kafkaReporter = new KafkaJsonReporter(config);
-		daemon.rps.add(kafkaReporter);
-	}
-
-	static public void addLoggerReporter(final Logger logR) {
-		Reporter logReport = new Reporter() {
-			@Override
-			public void report(Map data) {
-				logR.info(data.toString());
-			}
-		};
-		daemon.rps.add(logReport);
-	}
-
-	static public void addLoggerReporterByName(String name) {
-		final Logger logger = Logger.getLogger(name);
-		logger.setLevel(Level.INFO);
-		Reporter logReport = new Reporter() {
-			Logger logR = logger;
-
-			@Override
-			public void report(Map data) {
-				logR.info(data.toString());
-			}
-		};
-		daemon.rps.add(logReport);
-	}
-
-	static class MetricReportDaemon implements Runnable {
-
-		protected long peroid = 10; // 10 sencods
-		protected Set<Reporter> rps = Collections.synchronizedSet(new HashSet<Reporter>());
-
-		public void addReporter(Reporter r) {
-			if (r != null) {
-				this.rps.add(r);
-			}
-		}
-
-		public void setPeroid(long peroid) {
-			this.peroid = peroid;
-		}
-
-		@Override
-		public void run() {
-			while (true) {
-				try {
-					Thread.sleep(peroid * 1000);
-				} catch (Exception e) {
-				}
-				if (rps.isEmpty()) {
-					continue;
-				}
-				Map data;
-				for (MetricUnit u : method_dur.values()) {
-					try {
-						try {
-							data = u.sample();
-							data.put("client_id", u.clientId);
-							data.put("name", u.name);
-							data.put("group", u.group);
-						} catch (Exception e) {
-							continue;
-						}
-						for (Reporter r : rps) {
-							r.report(data);
-						}
-					} catch (Exception e) {
-						e.printStackTrace();
-						continue;
-					} finally {
-						data = null;
-					}
-				}
-			}
-		}
-	}
+//	static class MetricReportDaemon implements Runnable {
+//
+//		protected long peroid = 10; // 10 sencods
+//		protected Set<Reporter> rps = Collections.synchronizedSet(new HashSet<Reporter>());
+//
+//		public void addReporter(Reporter r) {
+//			if (r != null) {
+//				this.rps.add(r);
+//			}
+//		}
+//
+//		public void setPeroid(long peroid) {
+//			this.peroid = peroid;
+//		}
+//
+//		@Override
+//		public void run() {
+//			while (true) {
+//				try {
+//					Thread.sleep(peroid * 1000);
+//				} catch (Exception e) {
+//				}
+//				if (rps.isEmpty()) {
+//					continue;
+//				}
+//				Map data;
+//				for (MetricUnit u : method_dur.values()) {
+//					try {
+//						try {
+//							data = u.sample();
+//							data.put("client_id", u.clientId);
+//							data.put("name", u.name);
+//							data.put("group", u.group);
+//						} catch (Exception e) {
+//							continue;
+//						}
+//						for (Reporter r : rps) {
+//							r.report(data);
+//						}
+//					} catch (Exception e) {
+//						e.printStackTrace();
+//						continue;
+//					} finally {
+//						data = null;
+//					}
+//				}
+//			}
+//		}
+//	}
 }
 
 interface Reporter {
