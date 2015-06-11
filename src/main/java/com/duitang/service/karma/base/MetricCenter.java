5880e7097573e6bc0108c89570ffc45f60077741
Yan Su <yan.su@duitang.com>
2015-06-11 12:28:04 +0800

    location for MetricCenter

--- src/main/java/com/duitang/service/karma/base/MetricCenter.java
+++ src/main/java/com/duitang/service/karma/base/MetricCenter.java
@@ -1,13 +1,15 @@
 package com.duitang.service.karma.base;
 
+import java.lang.management.ManagementFactory;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ThreadLocalRandom;
 
+import io.netty.util.internal.StringUtil;
+import io.netty.util.internal.SystemPropertyUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -71,30 +73,78 @@ public class MetricCenter {
         metricUnitFor(name).record(elapse);
     }
 
-	private static class HostNameHolder {
-		public static final String hostname;
+	private static class LocationHolder {
+		public static final String HOSTNAME;
 		static {
 			String _hostname = null;
 			try {
 				_hostname = InetAddress.getLocalHost().getHostName();
-			} catch (UnknownHostException e) {
+			} catch (Exception e) {
 				logger.error("get hostname error", e);
 				_hostname = System.getenv("HOSTNAME");
                 if (_hostname == null || _hostname.isEmpty()) {
-                    _hostname = "gen-" + ThreadLocalRandom.current().nextLong(10000, 99999);
+                    _hostname = "unknown_" + ThreadLocalRandom.current().nextLong(10000, 99999);
                 }
 			}
-			hostname = _hostname;
+			HOSTNAME = _hostname;
 		}
-	}
 
+        public static final String APP_NAME;
+        static {
+            String _appName = null;
+            try {
+                _appName = SystemPropertyUtil.get("app.name");
+            } catch (Exception e) {
+                logger.error("get APP_NAME error", e);
+            }
+            if (_appName == null) {
+                _appName = "";
+            }
+            APP_NAME = _appName;
+        }
+
+        public static final String PID;
+        static {
+            String _pid = null;
+            try {
+                String name = ManagementFactory.getRuntimeMXBean().getName();
+                String[] split = StringUtil.split(name, '@');
+                if (split.length == 2) {
+                    _pid = split[1];
+                }
+            } catch (Exception e) {
+                logger.error("get PID error", e);
+            }
+            if (_pid == null) {
+                _pid = "";
+            }
+            PID = _pid;
+        }
+
+        public static final String LOCATION;
+        static {
+            StringBuilder b = new StringBuilder();
+            b.append(HOSTNAME);
+            if (!APP_NAME.isEmpty()) {
+                b.append('-').append(APP_NAME);
+            }
+            if (!PID.isEmpty()) {
+                b.append('-').append(PID);
+            }
+            LOCATION = b.toString();
+        }
+	}
 
     /**
-     *  hostname or randomly generated string
+     *  HOSTNAME or randomly generated string
      */
 	static public String getHostname() {
-		return HostNameHolder.hostname;
+		return LocationHolder.HOSTNAME;
 	}
+
+    static public String getLocation() {
+        return LocationHolder.LOCATION;
+    }
 
 	public static String genClientIdFromCode() {
 		StackTraceElement[] trac = Thread.currentThread().getStackTrace();
