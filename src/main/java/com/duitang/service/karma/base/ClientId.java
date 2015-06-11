1a36a15c62e0eac0df2b5542dbc80bcdad06bd03
Yan Su <yan.su@duitang.com>
2015-06-11 12:16:18 +0800

    MetricUnit/ClientId type

--- src/main/java/com/duitang/service/karma/base/ClientId.java
+++ src/main/java/com/duitang/service/karma/base/ClientId.java
@@ -3,28 +3,18 @@ package com.duitang.service.karma.base;
 public class ClientId {
     private final String name;
     private final boolean isClient;
-    private final String implName;
-    private final String location; // location of the process (host/rack/idc/pid)
 
     /**
      *
      * @param name      名字, 一般是 interface 全名
-     * @param implName  实现, server为实现名字, client为null
-     * @param location  地点, 可选, 提供 host/机柜/idc/进程 信息
+     * @param isClient  是否client
      */
-    public ClientId(String name, String implName, String location) {
+    public ClientId(String name, boolean isClient) {
         if(name == null) {
             throw new IllegalArgumentException("name==null");
         }
         this.name = name;
-        this.location = location;
-        if (implName == null || implName.isEmpty()) {
-            this.implName = null;
-            this.isClient = true;
-        } else {
-            this.implName = implName;
-            this.isClient = false;
-        }
+        this.isClient = isClient;
     }
 
     public String getName() {
@@ -33,13 +23,5 @@ public class ClientId {
 
     public boolean isClient() {
         return isClient;
-    }
-
-    public String getImplName() {
-        return implName;
-    }
-
-    public String getLocation() {
-        return location;
     }
 }
