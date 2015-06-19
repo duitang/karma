10137e3245334a20bfb6decca7714b3cdfb6c1f4
kevx <kevx@kevx-mac.local>
2015-06-19 16:58:42 +0800

    .

--- src/main/java/com/duitang/service/karma/client/KarmaIOPool.java
+++ src/main/java/com/duitang/service/karma/client/KarmaIOPool.java
@@ -85,9 +85,9 @@ public class KarmaIOPool implements LifeCycle {
 
 	protected GenericObjectPool<KarmaIoSession> forceCreatePool(String url) {
 		GenericObjectPoolConfig cfg = new GenericObjectPoolConfig();
-		cfg.setMaxIdle(100);
+		cfg.setMaxIdle(1);
 		cfg.setMinIdle(0);
-		cfg.setMaxTotal(300);
+		cfg.setMaxTotal(3);
 		cfg.setTestWhileIdle(false);
 		cfg.setBlockWhenExhausted(true);
 		cfg.setMaxWaitMillis(timeout);
