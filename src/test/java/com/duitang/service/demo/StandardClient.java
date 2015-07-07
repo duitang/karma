8189fe1ee1a7e45e015a73c5ee5b1b51ca9284f6
laurence <laurence@duitang.com>
2015-07-07 14:38:45 +0800

    test case service holder fix

--- src/test/java/com/duitang/service/demo/StandardClient.java
+++ src/test/java/com/duitang/service/demo/StandardClient.java
@@ -7,20 +7,24 @@ public class StandardClient {
 
 	public static void main(
 	        String[] args) {
+		
+		
 		RpcClientConfig cfg = new RpcClientConfig();
 		cfg.setAppName("mandala");
-		cfg.setGroup("japa");
+		cfg.setGroup("default_mandala");
 		cfg.setTimeout(5000);
-		cfg.setConnString("s15:3881");
+		cfg.setConnString("std-1.zk.infra.duitang.net:3881,std-2.zk.infra.duitang.net:3881,std-3.zk.infra.duitang.net:3881");
 		cfg.setUsingStaticRpcEndpoint(false);
-		cfg.setStaticRpcEndpoint("s29:11001");
-		
+		cfg.setStaticRpcEndpoint("");
+		cfg.init();
+
 		ServicesHolder service = new ServicesHolder();
 		service.setInterfaceName("com.duitang.service.biz.ICatService");
 		service.setRpcClientConfig(cfg);
 		service.init();
-		
-		service.create();
+
+		Object cli = service.create();
+		System.out.println(cli);
 	}
 
 }
