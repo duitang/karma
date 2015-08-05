45470aa2372ee698aaa5622fc47c784b6569e0fc
Yan Su <yan.su@duitang.com>
2015-08-05 11:07:25 +0800

    LOCATION as map

--- src/main/java/com/duitang/service/karma/stats/LocationHolder.java
+++ src/main/java/com/duitang/service/karma/stats/LocationHolder.java
@@ -1,8 +1,10 @@
 package com.duitang.service.karma.stats;
 
+import java.lang.management.ManagementFactory;
 import java.net.InetAddress;
-import java.util.concurrent.ThreadLocalRandom;
+import java.net.UnknownHostException;
 
+import com.google.common.collect.ImmutableMap;
 import io.netty.util.internal.SystemPropertyUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,17 +12,33 @@ import org.slf4j.LoggerFactory;
 public class LocationHolder {
     private static final Logger logger = LoggerFactory.getLogger(LocationHolder.class);
 
-    public static volatile String LOCATION;
+    public static volatile ImmutableMap<String, String> LOCATION;
     static String HOSTNAME = genHostName();
     private static String APP_NAME = genAppName();
+    private static long PID = genPID();
+
+    private static long genPID() {
+        long pid = -1;
+        try {
+            String name = ManagementFactory.getRuntimeMXBean().getName();
+            String[] split = name.split("@", 2);
+            if (split.length > 1) {
+                pid = Long.parseLong(split[0]);
+            }
+        } catch (Exception e) {
+            logger.error("get pid error", e);
+        }
+        return pid;
+    }
 
     private static String genHostName() {
+        String name = null;
         try {
-            return InetAddress.getLocalHost().getHostName();
-        } catch (Exception e) {
+            name = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
             logger.error("get hostname error", e);
         }
-        return "host-" + String.valueOf(ThreadLocalRandom.current().nextInt(10000,99999));
+        return name == null ? "" : name;
     }
 
     private static String genAppName() {
@@ -38,10 +56,12 @@ public class LocationHolder {
 
     public static void setHostname(String hostname) {
         HOSTNAME = hostname;
+        resetLocation();
     }
 
     public static void setAppName(String appName) {
         APP_NAME = appName;
+        resetLocation();
     }
 
     public static String getHostname() {
@@ -49,12 +69,11 @@ public class LocationHolder {
     }
 
     public static void resetLocation() {
-        StringBuilder b = new StringBuilder();
-        b.append(HOSTNAME);
-        if (!APP_NAME.isEmpty()) {
-            b.append('-').append(APP_NAME);
-        }
-        LOCATION = b.toString();
+        LOCATION = ImmutableMap.of(
+                "host", HOSTNAME,
+                "app", APP_NAME,
+                "pid", String.valueOf(PID)
+        );
     }
 
     static {
